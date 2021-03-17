090a45f1d6af0c2143c433b15b347c92d2777bdd
Marine Incera <marine.incera@gmail.com>
2021-03-17 20:31:07 +0100

    homepage: mobile homepage done

---
+++ src/Main/Homepage/Homepage.js
@@ -0,0 +1,18 @@
+
+import './homepage.css';
+import HomepageName from '../../images/commonImages/nameWithDate.jpg';
+import HomepageMario from '../../images/commonImages/homepageMario.jpg';
+
+function Homepage() {
+    return (
+     
+     <div className="homepage">
+         <img src={HomepageName} className="homepage-name"/>
+         <p className="press-button-mario">Click Here</p>
+         <img src={HomepageMario} className="homepage-mario"/>
+
+     </div>
+    );
+  }
+
+  export default Homepage
---
+++ src/Main/Main.js
@@ -0,0 +1,13 @@
+import Homepage from "./Homepage/Homepage";
+
+
+function Main() {
+    return (
+     
+     <div>
+         <Homepage></Homepage>
+     </div>
+    );
+  }
+
+  export default Main
