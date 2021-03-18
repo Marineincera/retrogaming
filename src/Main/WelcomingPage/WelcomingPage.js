f7e52c0cf988f741fb59ceaf5b14808a64c76455
Marine Incera <marine.incera@gmail.com>
2021-03-18 23:53:16 +0100

    welcoming page : get params et use them ok

--- src/Main/WelcomingPage/WelcomingPage.js
+++ src/Main/WelcomingPage/WelcomingPage.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./welcomingPage.css";
 import Typed from 'typed.js';
 import { withRouter } from 'react-router-dom';
+import Datas from '../../datas';
 
 class WelcomingPage extends React.Component {
     constructor(props){
         super(props)
-      }
+        this.selectedCharacter= {}
+    }
+      
 
     componentDidMount(){
-        let params = this.props.match.params
-        console.log(params);
         this.typed = new Typed('#welcomingTextFirstSetion', {
             strings: [
-                `^1000 Bonjour ${params.pseudo} !`, 
-                // `^1000 Bienvenue à toi et à ${this.props.selectedCharacter.name} sur mon cv interactif.`,
-                // `^1000 J'espère que cela te plaira.`,
+                `^1000 Bonjour ${this.props.match.params.pseudo} !`, 
+                `^1000 Bienvenue à toi et à ${this.selectedCharacter.name} sur mon cv interactif.`,
+                `^1000 J'espère que cela te plaira.`,
                 "N'hésite pas à me contacter pour plus d'informations.",
                 "Clique ici pour commencer"
             ], 
@@ -24,14 +25,17 @@ class WelcomingPage extends React.Component {
     }
     
     render(){    
-        return (
-            <div className="welcoming-page">
-                 <div className="welcoming-text-wrapper">
-                 <p id="welcomingTextFirstSetion" className="welcoming-text"></p>
-                </div>
-                    {/* <img src={this.props.selectedCharacter.fullimg} className="fullsize-character" /> */}
-                </div>
-        )      
+        let params = this.props.match.params
+        this.selectedCharacter = Datas.find(character => character.id == this.props.match.params.persoid);
+            return (
+                <div className="welcoming-page">
+                     <div className="welcoming-text-wrapper">
+                     <p id="welcomingTextFirstSetion" className="welcoming-text"></p>
+                    </div>
+                        <img src={this.selectedCharacter.fullimg} className="fullsize-character" />
+                    </div>
+            )      
+        
     }
 }
     
