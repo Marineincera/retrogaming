73c1d3a86cdca34e948cc3e78edca453e1835075
Marine Incera <marine.incera@gmail.com>
2021-03-18 22:45:55 +0100

    welcoming page done

--- src/Main/WelcomingPage/WelcomingPage.js
+++ src/Main/WelcomingPage/WelcomingPage.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "./welcomingPage.css";
+import Typed from 'typed.js';
 
 class WelcomingPage extends React.Component {
+    constructor(props){
+        super(props)
+      }
 
-        render(){
-        
-            return (
-                <div className="welcoming-page">
-                    <p>hello</p>
+    componentDidMount(){
+        this.typed = new Typed('#welcomingTextFirstSetion', {
+            strings: [
+                `^1000 Bonjour ${this.props.name} !`, 
+                `^1000 Bienvenue à toi et à ${this.props.selectedCharacter.name} sur mon cv interactif.`,
+                `^1000 J'espère que cela te plaira.`,
+                "N'hésite pas à me contacter pour plus d'informations.",
+                "Clique ici pour commencer"
+            ], 
+            typeSpeed: 50
+          });
+    }
+    
+    render(){    
+        return (
+            <div className="welcoming-page">
+                 <div className="welcoming-text-wrapper">
+                 <p id="welcomingTextFirstSetion" className="welcoming-text"></p>
                 </div>
-            )
-          
-        }
+                    <img src={this.props.selectedCharacter.fullimg} className="fullsize-character" />
+                </div>
+        )      
+    }
 }
     
     
