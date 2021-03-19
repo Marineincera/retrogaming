44176c8bae004ee8851b50277a6b035737519421
Marine Incera <marine.incera@gmail.com>
2021-03-19 19:54:55 +0100

    ResumePage: creation and navigation

---
+++ src/Main/ResumePage/ResumePage.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import { withRouter } from "react-router";
+import "./resumePage.css";
+
+class ResumePage extends React.Component {
+    constructor(props){
+        super(props)
+ 
+    }
+    
+    render(){
+        return(
+           <div className="resume-page">
+               <p>dd</p>
+            </div>
+        )
+    }
+        
+}
+
+export default withRouter(ResumePage)
