a939602b3fab171b1a43298f4bdabcb283dc98f3
Marine Incera <marine.incera@gmail.com>
2021-03-19 20:34:00 +0100

    resumePage: display menu titles

--- src/Main/ResumePage/ResumePage.js
+++ src/Main/ResumePage/ResumePage.js
@@ -8,15 +8,28 @@ import SmallSelectedCharacterComponent from "../commonComponent/SmallSelectedCha
 class ResumePage extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            menuTitles :["Presentation", "Experience Pro", "Formation", "+", "Contact"]
+        }
         this.selectedCharacter= {}
+        
     }
+
     
     render(){
         let params = this.props.match.params
         this.selectedCharacter = Datas.find(character => character.id == this.props.match.params.persoid);
         return(
            <div className="resume-page">
-               <p>dd</p>
+               <div className="menu-container">
+               {this.state.menuTitles.map((title) => {
+                        return (
+                            <div key={title} className="menu-title">
+                                <p>{title}</p>
+                            </div>
+                        )
+                    })}
+               </div>
                <SmallSelectedCharacterComponent selectedCharacter={this.selectedCharacter}/>
             </div>
         )
