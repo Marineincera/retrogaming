9edbd9c026049598f88bd4b1a94a6ee60c55d246
Marine Incera <marine.incera@gmail.com>
2021-03-21 00:52:43 +0100

    resumePage: skills datas added

---
+++ src/ResumeDatas/skillsDatas.js
@@ -0,0 +1,163 @@
+const skills = [
+    {
+        "title": "Web",
+        "skillsList": [
+                "Création d'application web et web-mobile JavaScript",
+                "Création de sites vitrines Wordpress & HTML/CSS",
+                "Création d'API REST en NodeJS - Express & TypeORM",
+                "Création de BDD SQL",
+                "Conception de Wireframes",
+                "Gestion du versionning Git - Github/GitLab",
+                "Création de vidéos (tournage et montage)",
+                "Choisir les outils web au service de la mise en oeuvre de la stratégie commerciale",
+                "Réalisation d'audits de sites internet (Qualité Tourisme™)"
+            ]
+    },
+    {
+        "title": "Marketing & Gestion de projet",
+        "skillsList": [
+                "Veille Marketing",
+                "Élaboration et mise en œuvre d’une politique commerciale",
+                "Diagnostic d’un territoire/ marché",
+                "Conception, gestion et contrôle de l’avancement d’un projet",
+                "Analyse et reporting des actions",
+                "Diagnostic d’un territoire/ d’un marché",
+                "Conception de projets de développement du tourisme local",
+                "Anticipation de l’impact d’un projet sur la stratégie de l’entreprise",
+                "Compréhension des impacts économiques du développement d’un projet",
+                "Conception et production événementielle",
+                "Gestion et suivi du budget des actions",
+                "Rédaction des cahiers des charges pour divers prestataires",
+                "Contrôle de l’avancement et de l’accomplissement des objectifs",
+                "Création d'affiches et de flyers",
+                "Retouche de photos",
+                "Animation de pages et comptes sur les réseaux sociaux",			
+            ]
+    },
+    {
+        "title": "Accueil & Vente",
+        "skillsList": [
+            "Accueil d'une clientèle française et internationale",
+            "Gestion d'une caisse",
+            "Répondre aux besoins d'une clientèle",
+            "Utilistion de logiciels de réservation"
+            ]
+    },
+    {
+        "title": "Outils numériques",
+        "skillsList": [          
+            {
+                "name" : "HTML/CSS",
+                "level" : "3",
+                "section": "Web Development"
+            },
+            {
+                "name" : "FlexBox",
+                "level" : "3",
+                "section": "Web Development"
+            },
+            {
+                "name" : "Word",
+                "level" : "3",
+                "section": "Tools"
+            },
+            {
+                "name" : "Excel",
+                "level" : "3",
+                "section": "Tools"
+            },
+            {
+                "name" : "Powerpoint",
+                "level" : "3",
+                "section": "Tools"
+            },
+            {
+                "name" : "Slack",
+                "level" : "3",
+                "section": "Tools"
+            },
+            {
+                "name" : "JavaScript",
+                "level" : "2",
+                "section": "Web Development"
+            },
+            {
+                "name" : "TypeScript",
+                "level" : "2",
+                "section": "Web Development"
+            },
+            {
+                "name" : "Github",
+                "level" : "2",
+                "section": "Web Development"
+            },
+            {
+                "name" : "Angular 9+",
+                "level" : "2",
+                "section": "Web Development"
+            },
+            {
+                "name" : "TypeORM",
+                "level" : "2",
+                "section": "Web Development"
+            },
+            {
+                "name" : "Trello",
+                "level" : "2",
+                "section": "Tools"
+            },
+            {
+                "name" : "React Native",
+                "level" : "1" ,
+                "section" : "Web Development"
+            },
+            {
+                "name" : "React Js",
+                "level" : "1",
+                "section" : "Web Development"
+            },
+            {
+                "name" : "CSS Grid",
+                "level" : "1",
+                "section": "Web Development"
+            },
+            {
+                "name" : "GitLab",
+                "level" : "1",
+                "section": "Web Development"
+            },
+            {
+                "name" : "Wordpress",
+                "level" : "1",
+                "section": "Web Development"
+            },
+            {
+                "name" : "SQL",
+                "level" : "1",
+                "section": "Web Development"
+            },
+            {
+                "name" : "Zapier",
+                "level" : "1",
+                "section": "Web Development"
+            },
+            {
+                "name" : "Photofiltre",
+                "level" : "1",
+                "section": "Communication"
+            },
+            {
+                "name" : "Windows Movie Maker",
+                "level" : "1",
+                "section": "Communication"
+            },
+            {
+                "name" : "InDesign",
+                "level" : "1",
+                "section": "Communication"
+            }        
+        ]
+    }
+]
+
+export default skills;
