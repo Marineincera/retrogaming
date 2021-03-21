deb0f0edd4da069005feb717b71c80ed832463c8
Marine Incera <marine.incera@gmail.com>
2021-03-21 19:46:47 +0100

    gamePage: selected character added

--- src/Main/GamePage/GamePage.js
+++ src/Main/GamePage/GamePage.js
@@ -1,6 +1,9 @@
 
 import React from "react";
 import "./gamePage.css";
+import { withRouter } from 'react-router-dom';
+import Datas from '../../datas';
+import SmallSelectedCharacterComponent from "../commonComponent/SmallSelectedCharacterComponent/SmallSelectedCharacterComponent";
 
 class GamePage extends React.Component {
     constructor(props){
@@ -8,18 +11,22 @@ class GamePage extends React.Component {
         this.state = {
           
         }
- 
+        this.params = this.props.match.params
+        this.selectedCharacter = {}
     }
 
 
     
     render(){
-
+        this.selectedCharacter = Datas.find(character => character.id == this.props.match.params.persoid);
+        console.log(this.selectedCharacter);
         return(
-            <div></div>
+            <div className="game-page">
+                <SmallSelectedCharacterComponent selectedCharacter={this.selectedCharacter}/>
+            </div>
         )
     }
         
 }
 
-export default GamePage
+export default withRouter(GamePage)
